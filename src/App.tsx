7c532938a0bbd2d8303301fb448d4ed8bfa8cfd1
arodriguez1996 <angel.rodriguez@kushkipagos.com>
2023-02-08 21:28:06 -0500

    enable backend connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {Container, ListGroup} from "react-bootstrap";
+import {get} from "lodash";
 
 function App() {
+
+  const [users, setUsers] = useState<any[]>([])
+
+  useEffect(() => {
+
+    (async () => {
+
+      const users = await axios.get(`${process.env.REACT_APP_API_URL}/users`)
+
+      setUsers(users.data)
+    })()
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container fluid >
+        <ListGroup>
+          {users.map((user) => <ListGroup.Item>{get(user, "firstName", "")} {get(user, "lastName", "")}</ListGroup.Item>)}
+        </ListGroup>
+      </Container>
   );
 }
 
